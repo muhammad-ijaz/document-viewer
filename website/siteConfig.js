cd8a94f1789c6c22a65a5f2cd1f3e94fb3f6042c
Alex Bobkov <lilalex85@gmail.com>
2020-01-10 12:31:47 +0300

    fixing baseUrl

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,14 +24,14 @@ const siteConfig = {
   title: 'GroupDocs', // Title for your website.
   tagline: 'Professional document manipulation APIs ',
   url: 'https://bobkovalex.github.io/document-viewer/', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  baseUrl: '/document-viewer/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'document-viewer',
+  organizationName: 'groupdocs',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
