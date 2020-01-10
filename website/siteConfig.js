15aface56e662fc3e100eb00b39ebe79a7edfee0
Alex Bobkov <lilalex85@gmail.com>
2020-01-10 12:17:16 +0300

    fixed url

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
   title: 'GroupDocs', // Title for your website.
   tagline: 'Professional document manipulation APIs ',
-  url: 'https://bobkovalex.github.io', // Your website URL
+  url: 'https://bobkovalex.github.io/document-viewer/', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
